4e51177c8c45535809ccbbf56c5b4b847fd3ac78
Remi DESCHAMPS <remi.deschamps@epitech.eu>
2020-04-08 09:42:51 -0400

    function

--- functions.py
+++ functions.py
@@ -4,6 +4,6 @@ import math
 
 def function(mean, deviation, x):
 
-    res = 0
+    res = (1 / (deviation * math.sqrt(2 * math.pi))) * math.exp(-math.pow((x - mean), 2)/math.pow((2 * deviation), 2))
 
     return (res)
